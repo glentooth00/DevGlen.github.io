49315010eddc97632e9ffbeac559b76ba118cced
Glen Donn B. Alpasan <62196184+glentooth00@users.noreply.github.com>
2023-01-11 10:43:33 +0800

    Add files via upload

---
+++ nav/nav.php
@@ -0,0 +1,21 @@
+<nav class="nav">
+    <ul>
+        <li>
+            <a href="#">HOME</a>
+        </li>
+        <li>
+            <a href="#">PROJECTS</a>
+        </li>
+        <li>
+            <a href="#">CONTACT</a>
+        </li>
+     
+       
+           
+        
+        
+    </ul>   
+
+    <span><img src="assets/images/icons/logo.png" id="logo"></span>
+
+</nav>
